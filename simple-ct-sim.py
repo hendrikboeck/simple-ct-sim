05ac6b150ce4d7c58fbf0040c7694687c6dc0366
Hendrik Boeck <hendrikboeck.dev@protonmail.com>
2022-05-28 12:23:31 +0200

    improve performance and fix rotating-artifact

--- simple-ct-sim.py
+++ simple-ct-sim.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 from PIL import Image
 from PIL.Image import Image as PILImage
 import numpy as np
@@ -19,22 +20,41 @@ async def ct_scan_from_angle(org: PILImage, angle: float) -> np.ndarray:
   return res
 
 
-def parallel_reconstruction(scans: np.ndarray, angle_step: float) -> PILImage:
-  normalized_scans = ((scans / np.amax(scans)) * 255.0) / float(len(scans))
-  img = Image.new("F", (IMG_SIZE, IMG_SIZE), 0)
+async def create_reprojection_array(
+    scl: np.ndarray, theta: float
+) -> np.ndarray:
+  des = np.empty((512, 512))
 
-  for r in range(len(normalized_scans)):
-    # rerotate image every iteration to prevent no rotation if angle is to small
-    img = img.rotate(r * angle_step)
-    for i in range(IMG_SIZE):
-      for j in range(IMG_SIZE):
-        img.putpixel(
-            (i, j),
-            img.getpixel((i, j)) + float(normalized_scans[r, j])
+  for i in range(512):
+    des[i] = scl
+
+  des = Image.fromarray(des)
+  des = des.rotate(theta - 90.0)
+
+  return np.asarray(des)
+
+
+async def parallel_reconstruction(
+    scans: np.ndarray, angle_step: float
+) -> PILImage:
+  scans = scans / np.amax(scans)
+  tasks = []
+
+  for i in range(len(scans)):
+    tasks.append(
+        asyncio.create_task(
+            create_reprojection_array(scans[i], i * angle_step)
         )
-    img = img.rotate(-1.0 * r * angle_step)
+    )
 
-  return img
+  des = np.zeros((IMG_SIZE, IMG_SIZE))
+  for t in tasks:
+    arr = await t
+    des += arr
+
+  des = np.flip(des, axis=1)
+  des = (des / np.amax(des)) * 255.0
+  return Image.fromarray(des)
 
 
 async def simulate_ct_scan(org: Image,
@@ -49,7 +69,8 @@ async def simulate_ct_scan(org: Image,
   for i, task in enumerate(tasks):
     scans[i] = await task
 
-  return scans, parallel_reconstruction(scans, angle_step)
+  des = await parallel_reconstruction(scans, angle_step)
+  return scans, des
 
 
 def ct_scan_thread(org: PILImage, num_pictures: int) -> None:
@@ -58,21 +79,15 @@ def ct_scan_thread(org: PILImage, num_pictures: int) -> None:
   reimg = reimg.convert("L")
   reimg.save(f"export/ct-sim-p{num_pictures}.jpg", "JPEG")
 
-  scbimg = Image.new("F", (IMG_SIZE, IMG_SIZE), 0)
-  for i in range(IMG_SIZE):
-    for j in range(IMG_SIZE):
-      scbimg.putpixel(
-          (i, j),
-          scbimg.getpixel((i, j)) +
-          float(raw[int((float(i) / IMG_SIZE) * len(raw)), j])
-      )
-  scbimg = scbimg.convert("L")
+  raw = (raw / np.amax(raw)) * 255.0
+  scbimg = np.zeros((512, 512))
+  for y in range(IMG_SIZE):
+    for x in range(IMG_SIZE):
+      scbimg[y, x] += float(raw[int((x / 512.0) * num_pictures), y])
+  scbimg = Image.fromarray(scbimg).convert("L")
   scbimg.save(f"export/ct-scb-p{num_pictures}.jpg", "JPEG")
 
-  reimg = reimg.convert("RGB")
-  scbimg = scbimg.convert("RGB")
   compimg = Image.open("import/graph.png").convert("RGB")
-
   compimg.paste(scbimg, (75, 100))
   compimg.paste(reimg, (662, 100))
   compimg.save(f"export/graph-p{num_pictures}.jpg", "JPEG")
@@ -81,9 +96,11 @@ def ct_scan_thread(org: PILImage, num_pictures: int) -> None:
 def main() -> None:
   org = Image.open("import/org.png").convert("L")
   threads = []
-  #runs = [1, 3, 4, 5, 8, 12, 16, 24, 32, 48, 64, 128, 256, 360, 720]
-  runs = [1, 3, 4, 5, 8, 12, 16, 24, 32, 48, 64, 128, 256]
+  runs = [1, 3, 4, 5, 8, 12, 16, 24, 32, 48, 64, 128, 256, 360, 720]
+  #runs = [1, 3, 4, 5, 8, 12, 16, 24, 32, 48, 64, 128, 256]
+  #runs = [24]
 
+  t0 = time.process_time()
   for i in runs:
     t = threading.Thread(target=ct_scan_thread, args=(
         org,
@@ -94,6 +111,7 @@ def main() -> None:
 
   for t in threads:
     t.join()
+  print(time.process_time() - t0)
 
 
 if __name__ == "__main__":
