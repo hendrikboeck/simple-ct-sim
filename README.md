462aa43c0b6c77945b53e3a1d8cdd19a54a7e2ef
Hendrik Boeck <hendrikboeck.dev@protonmail.com>
2022-05-30 14:58:36 +0200

    parameterize application, add license

--- README.md
+++ README.md
@@ -1,28 +1,67 @@
-# simple-ct-sim
+# Simple CT Simulation Python
+
+This program is written in Python and aims to simulate a CT imaging process in
+its most basic form. Therfore the input image is taken and scaned horizontally.
+The output of this operations will be ouput as scan-beam image with filename 
+`ct-scb-p{run}.jpg` or as part of `graph-p{run}.jpg`. No filters get applied to
+the reconstructed image. Only the histogram of the reconstruced image will be 
+streched to the borders.
+
+
 
 ## Usage
-
+Options:
 ```
+usage: simple-ct-sim.py [-h] --runs N [N ...] --input INPUT [--output OUTPUT]
+
+simple ct simulation
+
+options:
+  -h, --help        show this help message and exit
+  --runs N [N ...]  array of simulations that should be run. Number specifies the number of pictures taken in that simulation run.
+  --input INPUT     square graylevel, which CT should be simulated on
+  --output OUTPUT   output directory for images
+```
+
+## Getting started
+```
+$ git clone https://github.com/hendrikboeck/simple-ct-sim.git
+$ cd simple-ct-sim
 $ python3 -m venv env
-$ env/bin/activate
+$ ./env/bin/activate
 $ pip install -r requirements.txt
-$ python3 simple-ct-sim.py
+$ python3 simple-ct-sim.py --input import/org.png --ouput export --runs 24 32
 ```
 
-## Orginal Image
-<img src="import/org.png" height=400/>
+## Examples
 
+### Orginal Image
+<div align="center">
+  <img src="import/org.png" height=256 width=256/>
+</div>
 
-## Computed Images
+### Simulated CT Images
 
-### 4 Scans (90 deg)
-<img src=".github/md/graph-p4.jpg" height=400/>
+#### Run with 4 images taken (90 deg)
+```
+$ python3 simple-ct-sim.py --input import/org.png --ouput export --runs 4
+```
+<div align="center">
+  <img src=".github/md/graph-p4.jpg" height=356 width=712/>
+</div>
 
-### 8 Scans (45 deg)
-<img src=".github/md/graph-p8.jpg" height=400/>
+#### Run with 32 images taken (11.25 deg)
+```
+$ python3 simple-ct-sim.py --input import/org.png --ouput export --runs 32
+```
+<div align="center">
+  <img src=".github/md/graph-p32.jpg" height=356 width=712/>
+</div>
 
-### 32 Scans (11.25 deg)
-<img src=".github/md/graph-p32.jpg" height=400/>
-
-### 256 Scans (1.41 deg)
-<img src=".github/md/graph-p256.jpg" height=400/>
+#### Run with 256 images taken (1.41 deg)
+```
+$ python3 simple-ct-sim.py --input import/org.png --ouput export --runs 256
+```
+<div align="center">
+  <img src=".github/md/graph-p256.jpg" height=356 width=712/>
+</div>
--- simple-ct-sim.py
+++ simple-ct-sim.py
@@ -1,18 +1,29 @@
 import asyncio
 import time
 from PIL import Image
-from PIL.Image import Image as PILImage
 import numpy as np
 from typing import Tuple
 import threading
+from typing import NoReturn
+import sys
+import argparse
+import os
 
-IMG_SIZE = 512
+IMG_SIZE: int = 512  #px
+MAX_GRAY_VALUE: float = 255.0
+MIN_GRAY_VALUE: float = 0.0
+
+OUTPUT_DIR: str = "./export"
 
 
-async def ct_scan_from_angle(org: PILImage, angle: float) -> np.ndarray:
+def error_exit(*values: object) -> NoReturn:
+  sys.exit("ERROR: ", *values)
+
+
+async def ct_scan_from_angle(org: Image.Image, angle: float) -> np.ndarray:
   sub = org.rotate(angle)
-  sub = np.asarray(sub, dtype=float) / 255.0
-  res = np.zeros(IMG_SIZE, dtype=float)
+  sub = np.asarray(sub) / MAX_GRAY_VALUE
+  res = np.zeros(org.height)
 
   for i in range(len(sub)):
     res[i] = np.sum(sub[i])
@@ -23,9 +34,9 @@ async def ct_scan_from_angle(org: PILImage, angle: float) -> np.ndarray:
 async def create_reprojection_array(
     scl: np.ndarray, theta: float
 ) -> np.ndarray:
-  des = np.empty((512, 512))
+  des = np.empty((IMG_SIZE, IMG_SIZE))
 
-  for i in range(512):
+  for i in range(IMG_SIZE):
     des[i] = scl
 
   des = Image.fromarray(des)
@@ -36,7 +47,7 @@ async def create_reprojection_array(
 
 async def parallel_reconstruction(
     scans: np.ndarray, angle_step: float
-) -> PILImage:
+) -> Image.Image:
   scans = scans / np.amax(scans)
   tasks = []
 
@@ -49,18 +60,17 @@ async def parallel_reconstruction(
 
   des = np.zeros((IMG_SIZE, IMG_SIZE))
   for t in tasks:
-    arr = await t
-    des += arr
+    des += await t
 
   des = np.flip(des, axis=1)
-  des = (des / np.amax(des)) * 255.0
+  des = (des / np.amax(des)) * MAX_GRAY_VALUE
   return Image.fromarray(des)
 
 
 async def simulate_ct_scan(org: Image,
-                           num_pictures: int) -> Tuple[np.ndarray, PILImage]:
+                           num_pictures: int) -> Tuple[np.ndarray, Image.Image]:
   angle_step = 360.0 / num_pictures
-  scans = np.zeros((num_pictures, IMG_SIZE), dtype=float)
+  scans = np.zeros((num_pictures, IMG_SIZE))
   tasks = []
 
   for i in range(num_pictures):
@@ -73,35 +83,77 @@ async def simulate_ct_scan(org: Image,
   return scans, des
 
 
-def ct_scan_thread(org: PILImage, num_pictures: int) -> None:
+def ct_scan_thread(org: Image.Image, num_pictures: int) -> None:
+  t0 = time.process_time()
+
   raw, reimg = asyncio.run(simulate_ct_scan(org, num_pictures))
 
   reimg = reimg.convert("L")
-  reimg.save(f"export/ct-sim-p{num_pictures}.jpg", "JPEG")
+  reimg.save(f"{OUTPUT_DIR}/ct-sim-p{num_pictures}.jpg", "JPEG")
 
-  raw = (raw / np.amax(raw)) * 255.0
-  scbimg = np.zeros((512, 512))
+  raw = (raw / np.amax(raw)) * MAX_GRAY_VALUE
+  scbimg = np.zeros((IMG_SIZE, IMG_SIZE))
   for y in range(IMG_SIZE):
     for x in range(IMG_SIZE):
-      scbimg[y, x] += float(raw[int((x / 512.0) * num_pictures), y])
+      scbimg[y, x] += float(raw[int(float(x) / IMG_SIZE * num_pictures), y])
   scbimg = Image.fromarray(scbimg).convert("L")
-  scbimg.save(f"export/ct-scb-p{num_pictures}.jpg", "JPEG")
+  scbimg.save(f"{OUTPUT_DIR}/ct-scb-p{num_pictures}.jpg", "JPEG")
+
+  if IMG_SIZE != 512:
+    scbimg = scbimg.thumbnail((512, 512), Image.ANTIALIAS)
+    reimg = reimg.thumbnail((512, 512), Image.ANTIALIAS)
 
   compimg = Image.open("import/graph.png").convert("RGB")
   compimg.paste(scbimg, (75, 100))
   compimg.paste(reimg, (662, 100))
-  compimg.save(f"export/graph-p{num_pictures}.jpg", "JPEG")
+  compimg.save(f"{OUTPUT_DIR}/graph-p{num_pictures}.jpg", "JPEG")
+
+  dt = time.process_time() - t0
+  print(f"Thread #{num_pictures} completed! took {dt}s")
 
 
 def main() -> None:
-  org = Image.open("import/org.png").convert("L")
-  threads = []
-  runs = [1, 3, 4, 5, 8, 12, 16, 24, 32, 48, 64, 128, 256, 360, 720]
-  #runs = [1, 3, 4, 5, 8, 12, 16, 24, 32, 48, 64, 128, 256]
-  #runs = [24]
+  global IMG_SIZE, OUTPUT_DIR
 
-  t0 = time.process_time()
-  for i in runs:
+  parser = argparse.ArgumentParser(description="simple ct simulation")
+  parser.add_argument(
+      "--runs",
+      type=int,
+      nargs="+",
+      metavar="N",
+      help=
+      "array of simulations that should be run. Number specifies the number of"
+      " pictures taken in that simulation run.",
+      required=True
+  )
+  parser.add_argument(
+      "--input",
+      type=str,
+      help="square graylevel, which CT should be simulated on",
+      required=True
+  )
+  parser.add_argument(
+      "--output",
+      type=str,
+      help="output directory for images",
+      default=OUTPUT_DIR
+  )
+  args = parser.parse_args()
+
+  if not os.path.exists(args.output):
+    os.makedirs(args.output, exist_ok=True)
+  OUTPUT_DIR = args.output
+
+  if not os.path.exists(args.input):
+    error_exit("path to input file does not exist.")
+  org = Image.open(args.input).convert("L")
+  if org.height != org.width:
+    error_exit("image has to be square.")
+  IMG_SIZE = org.height
+
+  threads = []
+
+  for i in args.runs:
     t = threading.Thread(target=ct_scan_thread, args=(
         org,
         i,
@@ -111,7 +163,6 @@ def main() -> None:
 
   for t in threads:
     t.join()
-  print(time.process_time() - t0)
 
 
 if __name__ == "__main__":
